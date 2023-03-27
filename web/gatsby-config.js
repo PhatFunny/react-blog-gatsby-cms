ef90fff3297cd9e7732761a7b6df93c23767457f
PhatFunny <ntphat.funny@gmail.com>
2023-03-27 21:14:54 +0700

    react-helmet SEO

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -2,13 +2,15 @@ require('dotenv').config('./.env');
 
 module.exports = {
   siteMetadata: {
-    title: `phat-blog`,
+    title: `Phat Blog`,
     siteUrl: `https://techhub-blog.netlify.com`,
     description: `Phat Blog is a platform for latest technology news and updates.`,
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
   ],
 };
