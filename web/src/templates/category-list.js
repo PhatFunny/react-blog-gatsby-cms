711965340ef5a4fadca58712d52c90b57a1266e1
PhatFunny <ntphat.funny@gmail.com>
2023-04-03 07:38:24 +0700

    Add category list v2

--- web/src/templates/category-list.js
+++ web/src/templates/category-list.js
@@ -1,22 +1,56 @@
 import { graphql } from 'gatsby';
 import React from 'react';
+import PageSpace from '../components/PageSpace';
+import SEO from '../components/SEO';
+import PageHeader from '../components/PageHeader';
+import CategoryGrid from '../components/category/CategoryGrid';
+import Pagination from '../components/Pagination';
 
 export const query = graphql`
-  query categoriesQuery ($limit: Int!, $offset: Int!){
-    allSanityCategory {
-      sort: { fields: _createAt: order: DESC }
+  query categoriesQuery($limit: Int!, $offset: Int!) {
+    allSanityCategory(
+      sort: { fields: _createdAt, order: DESC }
       limit: $limit
       skip: $offset
-    }{
+    ) {
       nodes {
-        
+        id
+        title
+        slug {
+          current
+        }
+        _rawDescription
       }
     }
   }
 `;
 
-function CategoryList() {
-  return <div>CategoryList</div>;
+function CategoryList({ data, pageContext }) {
+  const { currentPage, numberOfPages } = pageContext;
+  const categories = data.allSanityCategory.nodes;
+
+  return (
+    <>
+      <SEO title="categories" />
+      <PageSpace top={80} bottom={100}>
+        <div className="container">
+          <PageHeader
+            title="All categories"
+            description="This month will bring about the 88th Academy Awards.
+          Starting in 1928, this prestigious award ceremony ..."
+          />
+          <CategoryGrid categories={categories} />
+          {numberOfPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              numberOfPages={numberOfPages}
+              baseURL="/categories"
+            />
+          )}
+        </div>
+      </PageSpace>
+    </>
+  );
 }
 
 export default CategoryList;
